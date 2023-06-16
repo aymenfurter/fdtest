936339a856473fd29d610a14846f1bacb220cdc5
Aymen Furter <aymen.furter@outlook.com>
2023-06-16 14:03:48 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,35 @@
 package main
 
 import (
-	"log"
-	"os"
+        "fmt"
+        "io/ioutil"
+        "log"
+        "os"
 )
 
 func main() {
-	for i := 0; ; i++ {
-		_, err := os.OpenFile("dummyFile.txt", os.O_RDONLY|os.O_CREATE, 0666)
-		if err != nil {
-			log.Fatalf("failed opening file: %s", err)
-		}
-	}
-}
+        var i uint64
+        var files []*os.File
 
+        for {
+                fileName := fmt.Sprintf("/tmp/dummyFile%d.txt", i)
+                file, err := os.OpenFile(fileName, os.O_RDONLY|os.O_CREATE, 0666)
+                if err != nil {
+                        log.Fatalf("Failed opening file: %s", err)
+                }
+
+                files = append(files, file)
+
+                if i%10000 == 0 {
+                        pid := os.Getpid()
+                        fdDir := fmt.Sprintf("/proc/%d/fd", pid)
+                        fds, err := ioutil.ReadDir(fdDir)
+                        if err != nil {
+                                log.Fatalf("Failed reading dir: %s", err)
+                        }
+
+                        log.Printf("Currently %d file descriptors are being used", len(fds))
+                }
+                i++
+        }
+}
